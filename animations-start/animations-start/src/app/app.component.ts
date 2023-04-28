2a6ca02c9a75919d21c24632474ffe526ed2781d
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2023-04-28 10:27:30 -0300

    finalizando a sessão de animação

--- animations-start/animations-start/src/app/app.component.ts
+++ animations-start/animations-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -91,10 +91,16 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
         ]))
       ]), //* é para o status inicial
       transition('* => void', [
-        animate(300, style({
-          transform: 'translateX(100px)',
-          opacity: 0
-        }))
+        group([
+          animate(800, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          })),
+          animate(300, style({
+            color: 'red'
+          }))
+        ])
+        
       ]),
     ]),
   ]
@@ -120,5 +126,13 @@ export class AppComponent {
 
   onShrink(){
     this.wildState = 'shrunken';
+  }
+
+  animationStarted(event){
+    console.log(event);
+  }
+
+  animationEnded(event){
+    console.log(event);
   }
 }
